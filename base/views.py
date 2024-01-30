6877e0d821332375c30f7e243bbfeceeb2f0b851
Johnny Trinh <johnny.trinh@hotmail.se>
2024-01-30 23:58:53 +0100

    Create navbar inside of template

--- base/views.py
+++ base/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return render(request, "home.html")
 
 def room(request):
-    return HttpResponse("This is the room view")
+    return render(request, "room.html")
