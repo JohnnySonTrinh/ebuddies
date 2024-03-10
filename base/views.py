b3c2a24171251dc8d65727f4f6dc3a0c7ac40b87
JohnnySonTrinh <139331088+JohnnySonTrinh@users.noreply.github.com>
2024-03-10 16:23:39 +0100

    Fix thread issue [Live]

--- base/views.py
+++ base/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render
+from .models import Thread
 
 # Create your views here.
 
-threads = [
-    {'id': 1, 'name': 'League of Legends'},
-    {'id': 2, 'name': 'Valorant'},
-    {'id': 3, 'name': 'Teamfight Tactics'},
-]
+#threads = [
+#    {'id': 1, 'name': 'League of Legends'},
+#    {'id': 2, 'name': 'Valorant'},
+#    {'id': 3, 'name': 'Teamfight Tactics'},
+#]
 
 def home(request):
+    threads = Thread.objects.all()
     context = {'threads': threads}
     return render(request, 'base/home.html', context)
 
 def thread(request, pk):
-    thread = None
-    for i in threads:
-        if i['id'] == pk:
-            thread = i
+    thread = Thread.objects.get(id=pk)
     context = {'thread': thread}
     return render(request, 'base/thread.html', context)
