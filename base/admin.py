419f89e7b69b23743f621b9229dae2764b2144d6
JohnnySonTrinh <139331088+JohnnySonTrinh@users.noreply.github.com>
2024-03-10 20:39:56 +0100

    Add topic, thread and message to admin panel

--- base/admin.py
+++ base/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Thread
+from .models import Thread, Topic, Message
 
 admin.site.register(Thread)
+admin.site.register(Topic)
+admin.site.register(Message)
